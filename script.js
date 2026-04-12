f37549be4f453e44012c32310bc2c45e84c1ebb2
GameDevKasinath <113451664+GameDevKasinath@users.noreply.github.com>
2026-04-12 22:49:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ const nav = document.querySelector(".nav");
 window.addEventListener("scroll", () => {
   if (window.scrollY > 50) {
     nav.style.background = "rgba(255,255,255,0.9)";
-    nav.style.backdropFilter = "blur(10px)";
   } else {
-    nav.style.background = "transparent";
+    nav.style.background = "rgba(255,255,255,0.6)";
   }
 });
