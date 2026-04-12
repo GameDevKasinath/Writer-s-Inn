f0b2d23666a5748183d295a85705ed09f81dacb0
GameDevKasinath <113451664+GameDevKasinath@users.noreply.github.com>
2026-04-12 23:00:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-const nav = document.querySelector(".nav");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY > 50) {
-    nav.style.background = "rgba(255,255,255,0.9)";
-  } else {
-    nav.style.background = "rgba(255,255,255,0.6)";
-  }
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
 });
