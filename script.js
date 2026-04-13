343cd40ceb226c5572442b3c14f2c1a278f56417
GameDevKasinath <113451664+GameDevKasinath@users.noreply.github.com>
2026-04-13 22:22:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-// SCROLL ANIMATION
+// SCROLL FADE
 const fades = document.querySelectorAll(".fade");
 
 window.addEventListener("scroll", () => {
+
   fades.forEach(el => {
     if (el.getBoundingClientRect().top < window.innerHeight - 100) {
       el.classList.add("show");
     }
   });
+
+  // 🔥 HERO OVERLAY EFFECT
+  const overlay = document.getElementById("overlay");
+  const scrollY = window.scrollY;
+
+  overlay.style.background = `rgba(0,0,0,${scrollY / 600})`;
+
 });
 
 // MOBILE MENU
-function toggleMenu() {
+function toggleMenu(){
   const menu = document.getElementById("mobileMenu");
   menu.style.display = menu.style.display === "flex" ? "none" : "flex";
 }
